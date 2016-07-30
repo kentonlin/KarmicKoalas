1de4405cdabee83a92441f663530058d7da03deb
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-07-30 11:06:25 -0400

    can draw path

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,5 +1,7 @@
 
 import React, { Component } from 'react'
+import haversine from 'haversine'
+import pick from 'lodash/pick'
 import {
   AppRegistry,
   StyleSheet,
@@ -17,27 +19,42 @@ class Main extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { routeCoordinates: [] }
+    this.state = {
+      routeCoordinates: [],
+      distanceTravelled: 0,
+      prevLatLng: {}
+     }
   }
 
   componentDidMount() {
   // StatusBarIOS.setStyle('light-content')
 
   navigator.geolocation.getCurrentPosition(
-    (position) => alert(position),
+    (position) => console.log(position),
     (error) => alert(error.message),
     {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
   )
-  // this.watchID = navigator.geolocation.watchPosition((position) => {
-  //   const { routeCoordinates } = this.state
-  //   const positionLatLngs = pick(position.coords, ['latitude', 'longitude'])
-  //   this.setState({ routeCoordinates: routeCoordinates.concat(positionLatLngs) })
-  // });
+  this.watchID = navigator.geolocation.watchPosition((position) => {
+    console.log(position);
+    const { routeCoordinates, distanceTravelled } = this.state
+     const newLatLngs = {latitude: position.coords.latitude, longitude: position.coords.longitude }
+    const positionLatLngs = pick(position.coords, ['latitude', 'longitude'])
+    this.setState({
+        routeCoordinates: routeCoordinates.concat(positionLatLngs),
+        distanceTravelled: distanceTravelled + this.calcDistance(newLatLngs),
+        prevLatLng: newLatLngs
+     })
+    console.log(this.state.distanceTravelled);
+  });
 }
 
-componentWillUnmount() {
-  navigator.geolocation.clearWatch(this.watchID);
-}
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+  calcDistance(newLatLng) {
+     const { prevLatLng } = this.state
+     return (haversine(prevLatLng, newLatLng) || 0)
+   }
 
 
   render() {
@@ -48,6 +65,11 @@ componentWillUnmount() {
 
           showsUserLocation={true}
           followUserLocation={true}
+          overlays={[{
+            coordinates: this.state.routeCoordinates,
+            strokeColor: 'blue',
+            lineWidth: 8,
+          }]}
         />
         <View style={styles.navBar}><Text style={styles.navBarText}>Killa Koala</Text></View>
       </View>
