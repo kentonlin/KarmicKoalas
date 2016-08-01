e6f6eb92bd8e718c23e517c2424aa8c3842b5b62
RebeccaGray <rebecca.gray@gmail.com>
2016-08-01 14:36:44 -0400

    sending group data back to user on socket

--- server/server.js
+++ server/server.js
@@ -10,17 +10,22 @@ app.use(express.static('../'));
 // app.use(bodyParser.json());
 
 server.listen(3001);
-
+console.log('server listening on ' + '3001')
 // use socket.io
 var io = require('socket.io').listen(server);
+
 
 //turn off debug
 io.set('log level', 1);
 
 // define interactions with client
 io.sockets.on('connection', function(socket){
+
      socket.on('location', function(data) {
       console.log("Incoming location:", data)
+
+      //sending dummy data for group update 
+      socket.emit('groupUpdate', {'group':[{'latitude': data.coordinates.latitude, 'longitude':  data.coordinates.longitude, 'title': 'Konst' }, {'latitude':data.coordinates.latitude + 0.0008, 'longitude': data.coordinates.longitude, 'title': 'Bo' }]});
     });
     socket.on('error', function(err) {
       console.log("Error", err);
