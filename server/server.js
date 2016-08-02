76aa12b5c37f569cfa73fd3d62dda04583a0ade8
RebeccaGray <rebecca.gray@gmail.com>
2016-08-02 07:17:21 -0400

    deploy

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
+  .use((req, res) => res.sendFile(../test) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 //const api = require('./api')
