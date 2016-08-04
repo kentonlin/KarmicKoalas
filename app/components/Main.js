16e0d23242c03189b53822a0a5c6cd1d35066a45
MKS-students <mks.students@hackreactor.com>
2016-08-04 19:59:44 -0400

    navIOS initial-Main -> SignUp

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,10 @@
 'use strict'
 
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, MapView, TextInput, Dimensions, StatusBarIOS, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Text, MapView, TextInput, Dimensions, StatusBarIOS, TouchableHighlight, AsyncStorage } from 'react-native';
 import Chat from './chat'
 import MapComponent from './Map.js'
+import SignUp from './SignUp'
 
 if (window.navigator && Object.keys(window.navigator).length == 0) {
   window = Object.assign(window, { navigator: { userAgent: 'ReactNative' }});
@@ -23,7 +24,21 @@ class Main extends Component {
     this.state = {
       eventId: '1',//eventId: props.eventId,   //this will come from group list view and pass to server
       socket:this.socket
-     }
+    }
+    AsyncStorage.setItem("signedUp", "false");
+    AsyncStorage.getItem("signedUp").then((value) => {
+      if(value !== "true"){
+        this.renderSignUp();
+      console.log("signedUp:", value)
+      }
+    });
+  }
+
+  renderSignUp(){
+    this.props.navigator.push({
+      component: SignUp,
+      title: "Sign Up"
+    });
   }
 
    componentDidMount() {
@@ -36,6 +51,16 @@ class Main extends Component {
       <View style={styles.container}>
         <MapComponent socket={this.state.socket}/>
         <Chat socket={this.socket}/>
+        <TouchableHighlight
+          style={styles.button1}
+          onPress={() => {alert('Hello1')}}>
+          <Text>Create new pin</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.button2}
+          onPress={() => {alert('Hello2')}}>
+          <Text>Create new pin</Text>
+        </TouchableHighlight>
       </View>
     )
   }
@@ -46,6 +71,23 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  button1: {
+    flex: 1,
+    bottom:0,
+    position: 'absolute',
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingVertical: 12,
+  },
+  button2: {
+    flex: 1,
+    bottom: 0,
+    left: 120,
+    position: 'absolute',
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingVertical: 12
   }
 })
 
