595b3bf8200670f8f2ab8cbe02eeab0f051829c8
RebeccaGray <rebecca.gray@gmail.com>
2016-08-06 11:28:18 -0400

    models for join tables

--- server/db/models/keyword.js
+++ server/db/models/keyword.js
@@ -2,7 +2,7 @@ var db = require('../config');
 var Route = require('./route');
 
 var Keyword = db.Model.extend({
-  tableName: 'keyword',
+  tableName: 'keywords',
      routes: ()=> {
      return this.belongsToMany(Route);
    },
---
+++ server/db/models/keyword_route.js
@@ -0,0 +1,18 @@
+var db = require('../config');
+var Users = require('./user');
+var Events = require('./event');
+
+var event_participant = db.Model.extend({
+  tableName: 'event_participant',
+     events_id: ()=> {
+     return this.belongsToMany(Events);
+   },
+   participant: ()=> {
+     return this.belongsToMany(Users);
+   },
+  hasTimestamps: true
+});
+
+module.exports = event_participant;
+
+
