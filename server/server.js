f7a446083419167bd8f79a1ae59a92b6a0fda729
RebeccaGray <rebecca.gray@gmail.com>
2016-08-12 19:12:08 -0400

    fix for getRouteById

--- server/server.js
+++ server/server.js
@@ -16,13 +16,35 @@ var userId;
 var username;
 var myRoom;
 
+const db = require('./db/config');
+
+var joinRoom = (data, socket) => {
+  var myRoom = data.eventId;
+  //check to see if user_id is matched with event_id in
+  //events_participants join table
+  return db.knex.raw('SELECT * FROM `events_participants` WHERE `event_id`=' + myRoom + ' AND `user_id`= ' + data.user_id)
+  .then((result) => {
+    console.log(result)
+    // if (result)
+    //   if(rooms.myRoom){
+    //     //existing room
+    //     rooms.myRoom.push(socket)
+    //   } else {
+    //     rooms.myRoom = [];
+    //     rooms.myRoom.push(socket)
+    //   }
+       return;
+  })
+
+}
 io.on('connection', (socket) => {
             console.log('Client connected');
 
             socket.on('intitialize', (data) => {
-                var myRoom = toString(data.eventId);
-                var userId = toString(data.userId);
-                var username = toString(data.username);
+                myRoom = data.eventId;
+                userId = data.userId;
+                username = data.username;
+                joinRoom(myRoom, socket)
                 console.log('intialaze client side', data)
                 socket.join(myRoom);
             });
