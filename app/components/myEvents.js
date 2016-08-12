27cd7f6a420c3b7018c7db1b3975c268803874d6
MKS-students <mks.students@hackreactor.com>
2016-08-12 16:11:26 -0400

    spaces

--- app/components/myEvents.js
+++ app/components/myEvents.js
@@ -55,7 +55,7 @@ class myEvents extends Component {
     //     objectdataSource: dataSource.cloneWithRows(JSON.parse(responseData._bodyText))
     //   });
     // }).done();
-  });
+    });
   }
 
   goMap(rowData) {
