43c7ef1d9d3daf5c7a6c181c5f4ab1211751c885
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-08-18 19:57:49 -0400

    live links

--- app/components/myEvents.js
+++ app/components/myEvents.js
@@ -26,7 +26,7 @@ class myEvents extends Component {
   }
 
   componentDidMount(){
-    fetch("http:localhost:8000/getMyEvents", {
+    fetch("https://wegotoo.herokuapp.com/getMyEvents", {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({user_id: this.props.userId})
