aab4ede20f157f901f384397845073604e246f2c
Konstantin Zamaraev <zamaraevk@gmail.com>
2016-08-17 23:27:31 -0400

    my events

--- app/components/searchRoutes.js
+++ app/components/searchRoutes.js
@@ -41,7 +41,7 @@ class SearchRoutes extends Component {
   }
 
   getRoutes(){
-    fetch("http://localhost:8000/searchKeywords", {
+    fetch("https://wegotoo.herokuapp.com/searchKeywords", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({keywords: this.state.search.trim().split(',')})
